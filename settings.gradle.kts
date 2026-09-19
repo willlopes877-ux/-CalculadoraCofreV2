a9a510b58732dec4ed65844990a3820c427b3f2c
willlopes877-ux <willlopes877@gmail.com>
2026-09-19 11:19:50 -0300

    Atualizar o settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,18 @@
-settings.gradle.kts
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CalculadoraCofreV2"
+include(":app")

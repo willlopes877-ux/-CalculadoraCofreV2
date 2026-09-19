1f68ef9746a442459cd18ac81a9d80457a3ea5f1
willlopes877-ux <willlopes877@gmail.com>
2026-09-19 11:44:08 -0300

    Atualizar o build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,28 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.calculatorvault"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.calculatorvault"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "2.0"
+    }
+}
+
+configurations.all {
+    resolutionStrategy {
+        force("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
+        force("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.22")
+        force("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.22")
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
 }
